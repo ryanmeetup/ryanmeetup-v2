4b15700cf92426091556cfdf80b2597dbf560183
Ryan Collins <github@rc.ryancollins.org>
2026-03-29 22:34:58 -0400

    HTML entities required?

--- components/navigation/Banner.tsx
+++ components/navigation/Banner.tsx
@@ -7,7 +7,7 @@ import { layoutPaddingX } from "@/lib/constants";
 const Banner = () => {
   return (
     <div className={`font-cooper text-white  bg-[#073326] py-1 ${layoutPaddingX}`}>
-      We're heading to Minneapolis on Saturday, July 25 for the Ryan Baseball Classic! See you there, Ryan.{" "}
+      We&apos;re heading to Minneapolis on Saturday, July 25 for the Ryan Baseball Classic! See you there, Ryan.{" "}
       <NextLink href="/rsvp" className="underline hover:text-[#ad8f4f]">
         RSVP today
       </NextLink>
