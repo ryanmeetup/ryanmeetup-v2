f1d00dfc68cf59f3642cbfe8a69ca5e1ab8e8912
Ryan Le <ryanle@live.com>
2026-05-02 12:12:40 -0400

    feat: update newsletter link

--- app/newsletter/page.tsx
+++ app/newsletter/page.tsx
@@ -2,15 +2,15 @@
 import { createRedirectPage } from "@/utils/redirect";
 
 const { metadata, RedirectPage } = createRedirectPage({
-  url: "https://docs.google.com/forms/d/e/1FAIpQLSdS8O47kdOcmjXglOt_aGTs2q9qK6CrN4zGFdx62H10a-8kDg/viewform",
+  url: "https://ryanmeetup.kit.com/49a74eff6b",
   metadata: {
     title: "Ryan Meetup - Newsletter",
     description: "Subscribe to the official Ryan Meetup newsletter.",
     canonical: "https://ryanmeetup.com/newsletter",
     image: {
       url: "https://ryanmeetup.com/meta/newsletter.png",
-      width: 798,
-      height: 373,
+      width: 1760,
+      height: 912,
     },
     keywords: [
       "ryan meetup newsletter",
