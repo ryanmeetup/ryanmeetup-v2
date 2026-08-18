2143cb82b748e1c29214f83ccee344a4a528fcc2
Ryan Le <ryanle@live.com>
2026-08-17 22:03:37 -0500

    chore: update /cards page with new vendor info

--- apps/ryanmeetup/components/cards/CardInfo.tsx
+++ apps/ryanmeetup/components/cards/CardInfo.tsx
@@ -1,8 +1,12 @@
 // Components
 import { Button, Card, Heading, Text } from "@ryanmeetup/ui";
+import NextLink from "next/link";
 import { FaDollarSign as Dollar } from "react-icons/fa";
 
 const CardInfo = () => {
+  const hrefStyles =
+    "font-semibold text-black/80 underline decoration-black/30 underline-offset-4 hover:decoration-black/60 dark:text-white/80 dark:decoration-white/30 dark:hover:decoration-white/60";
+
   return (
     <Card
       variant="solid"
@@ -18,22 +22,29 @@ const CardInfo = () => {
 
       <section className="space-y-4 text-base text-white/80 lg:text-lg">
         <Text>
-          Each purchase includes a customized anodized aluminum card that was
-          designed by a Ryan and produced by a Ryan—just for you, Ryan.
+          Each purchase includes a customized anodized aluminum card that was designed by Ryan and produced by Ryan — just for you, Ryan. Feast your eyes on your unique RY-D.  
         </Text>
 
         <Text>
           But it&apos;s more than just another flex on your non-Ryan friends.
           Your card comes with Ryan Pre-Check, letting you breeze past
-          secu-Ry-ty at our events without having to show your Ry-D.
+          secu-Ry-ty at our events without having to show your government ID.
+        </Text>
+
+        <Heading className="text-2xl title text-white" size="h2">
+          Powered by a Ryan-Owned Business
+        </Heading>
+
+        <Text>
+          We&apos;re proud to be partnering with fellow Ryan, Ryan Toney, and his team at <NextLink className={hrefStyles} href='https://www.pmapparel.com/'>P&M Apparel</NextLink> (based in Polk City, IA) to produce and fulfill these premium, everyday-carry cards.
         </Text>
 
         <div className="text-white/70 tracking-wide font-medium mt-8">
           Price:{" "}
           <span className="relative group font-bold text-white hover:underline">
-            $30 USD
+            $35 USD
           </span>
-          ; includes free shipping.
+          ; includes free shipping across the US. Additional rates may apply for international shipping.
         </div>
       </section>
 
@@ -45,8 +56,7 @@ const CardInfo = () => {
         <Text>
           This price allows us to cover the cost of production, shipping, and
           handling, while also supporting the ongoing development of the Ryan
-          Meetup community. It also supports Ryan&apos;s work at Transition
-          Marketing.
+          Meetup community. It also supports Ryan&apos;s work at P&M Apparel.
         </Text>
 
         <Text>
